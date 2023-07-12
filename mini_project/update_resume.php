9a5125e6a2c7fc3a1eb9f687d599e0f231001bed
tarun15082003 <nagireddy_2101cs48@iitp.ac.in>
2023-07-12 21:39:32 +0530

    removing mini_project

--- mini_project/update_resume.php
+++
@@ -1,5 +0,0 @@
-<form action="submit_resume.php" method="post" enctype="multipart/form-data">
-  Select PDF file to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload PDF" name="submit">
-</form>
